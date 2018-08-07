bc50c17c005a99c50934115a640427d950a69fa9
Flavio Amaral <flaviomirai@gmail.com>
2018-08-07 06:38:47 -0300

    Povoando dinamimcante estado e cidade

--- src/services/domain/cidade.service.ts
+++ src/services/domain/cidade.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
-import { EstadoDTO } from "../../models/estado.dto";
 import { Observable } from "rxjs/Rx";
+import { CidadeDTO } from "../../models/cidades.dto";
 
 @Injectable()
-export class EstadoService {
+export class CidadeService {
 
     constructor(public http: HttpClient) {
     }
 
-    findAll() : Observable<EstadoDTO[]>  {
-        return this.http.get<EstadoDTO[]>(`${API_CONFIG.baseUrl}/estados`);
+    findAll(estado_id : string) : Observable<CidadeDTO[]>  {
+        return this.http.get<CidadeDTO[]>(`${API_CONFIG.baseUrl}/estados/${estado_id}/cidades`);
     }
 }
