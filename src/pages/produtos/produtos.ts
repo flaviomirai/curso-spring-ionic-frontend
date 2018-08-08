8e9c394f8ae3c3d8ad1cb4d831d63dfa316e2027
Flavio Amaral <flaviomirai@gmail.com>
2018-08-07 21:40:18 -0300

    Carregando imagens dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -21,13 +22,28 @@ export class ProdutosPage {
   }
 
   ionViewDidLoad() {
-    let categoria_id = this.navParams.get('categoria_id');
+      let categoria_id = this.navParams.get('categoria_id');
 
-    this.produtoService.findByCategoria(categoria_id).subscribe(response => {
-      this.items = response['content'];
-    },
-    error => {}
-  );
+      this.produtoService.findByCategoria(categoria_id).subscribe(response => {
+        this.items = response['content'];
+        this.loadImageUrls();
+      },
+      error => {}
+    );
   }
+
+  loadImageUrls() {
+    this.items.forEach(item => {
+      this.produtoService.getSmallImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+        },
+        error => {});
+    });
+
+  }  
+
+
+
 
 }
