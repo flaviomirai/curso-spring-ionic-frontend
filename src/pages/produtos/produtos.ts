28cf670a5d3d3d121b75cfe9420344d08b8a7d7f
Flavio Amaral <flaviomirai@gmail.com>
2018-08-09 06:57:56 -0300

    Infinit Scroll

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -12,7 +12,8 @@ import { API_CONFIG } from '../../config/api.config';
 export class ProdutosPage {
 
   
-  items: ProdutoDTO[];
+  items: ProdutoDTO[] = []; // comeca vazio para concatenar a paginação
+  page: number = 0;
 
   constructor(
     public navCtrl: NavController, 
@@ -30,11 +31,14 @@ export class ProdutosPage {
     let categoria_id = this.navParams.get('categoria_id');
 
     let loader = this.presentLoadingDefault();
-    this.produtoService.findByCategoria(categoria_id).subscribe(response => {
-      this.items = response['content'];
+    this.produtoService.findByCategoria(categoria_id, this.page, 10).subscribe(response => {
+      let start = this.items.length;
+      this.items = this.items.concat(response['content']); // concatenando a nova resposta com o que estava antes
+      let end = this.items.length - 1;
       loader.dismiss();
-      this.loadImageUrls();
-      
+      console.log(this.page);
+      console.log(this.items);
+      this.loadImageUrls(start, end);    
     },
     error => {
       loader.dismiss();
@@ -42,14 +46,16 @@ export class ProdutosPage {
   );
   }
 
-  loadImageUrls() {
-    this.items.forEach(item => {
+  loadImageUrls(start: number, end: number) {
+
+    for (var i=start; i<=end; i++) {
+      let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
         .subscribe(response => {
           item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
         },
         error => {});
-    });
+    }
   }  
 
   showDetail(produto_id: string){
@@ -65,10 +71,20 @@ export class ProdutosPage {
   }
 
   doRefresh(refresher) {
+    this.page = 0;
+    this.items = [];
     this.loadData();
     setTimeout(() => {
       refresher.complete();
     }, 1000);
+  }
+
+  doInfinite(infiniteScroll) {
+    this.page++;
+    this.loadData();
+    setTimeout(() => {
+      infiniteScroll.complete();
+    }, 500);
   }
 
 
