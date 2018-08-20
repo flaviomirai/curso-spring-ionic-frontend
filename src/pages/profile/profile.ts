1349cf96143d48583e6a85834a5964e64ee67a4c
Flavio Amaral <flaviomirai@gmail.com>
2018-08-20 14:47:33 -0300

    Bugfix - atualização da imagem de perfil

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -5,6 +5,8 @@ import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.servcice';
 import { API_CONFIG } from '../../config/api.config';
 import { CameraOptions, Camera } from '@ionic-native/camera';
+import { PromiseObservable } from '../../../node_modules/rxjs/observable/PromiseObservable';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @IonicPage()
 @Component({
@@ -16,6 +18,8 @@ export class ProfilePage {
   cliente: ClienteDTO;
   picture: String;
   cameraOn: boolean = false;
+  profileImage: any;
+  
 
 
   constructor(
@@ -23,8 +27,10 @@ export class ProfilePage {
     public navParams: NavParams, 
     public storage: StorageService,
     public clienteService: ClienteService,
-    public camera: Camera
+    public camera: Camera,
+    public sanitizer: DomSanitizer
   ) {
+    this.profileImage = 'assets/imgs/avatar-blank.png';
   }
 
   ionViewDidLoad() {
@@ -53,9 +59,25 @@ export class ProfilePage {
   getImageIfExists(){
     this.clienteService.getImageFromBucket(this.cliente.id).subscribe(
       response => {
-        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`
+        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+        this.blobToDataUrl(response).then(dataUrl => {
+          let str : string = dataUrl as string;
+          this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str); // fala explicitamente que nao tem perigo
+        });
+      },
+      error => {
+        this.profileImage = 'assets/imgs/avatar-blank.png';
       }
     )
+  }
+
+  blobToDataUrl(blob){ // convete binario para string base 64
+    return new Promise((fulfill, reject) => {
+      let reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = (e) => fulfill(reader.result);
+      reader.readAsDataURL(blob);
+    })
   }
 
   getCameraPicture(){
@@ -75,10 +97,28 @@ export class ProfilePage {
     });
   }
 
+  getGalleryPicture(){
+    this.cameraOn = true;
+    const options: CameraOptions = {
+      quality: 100,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY ,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     this.picture = 'data:image/png;base64,' + imageData;
+     this.cameraOn = false;
+    }, (err) => {
+      this.cameraOn = true;
+    });
+  }
+
   sendPicture(){
     this.clienteService.uploadPicture(this.picture).subscribe(response => {
       this.picture = null;
-      this.loadData();
+      this.getImageIfExists();
     },
     error => {}
   );
